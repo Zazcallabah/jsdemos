8ee9fd2aedee5a9cc7ffe2ab8154a66ce96f3375
Peter Hamberg <zazcallabah@gmail.com>
2012-05-25 13:58:09 +0200

    fixed magnetic field, fixed positioning of particles, fixed viewport position

--- particle/moon.js
+++ particle/moon.js
@@ -22,18 +22,11 @@ var makeRotational = function( theta, type )
 		vec({x:0,y:cos,z:sin}),
 		vec({x:0,y:-1*sin,z:cos})];
 };
-var makeScaler = function()
-{
-	return {
-		dist: function( x ){ return x/1e5; },
-		time: function( t ) { return t/10000; }
-	};
-};
 
 var makeMoon = function()
 {
+	var position = vec({x:3.62570e8,y:0,z:0});
 	var radius = 1.7371e6;
-	var position = vec();
 	return {
 		r: function() {return radius;},
 		tick: function(){},
@@ -41,7 +34,17 @@ var makeMoon = function()
 		style: function(){return "white";}
 	};
 };
-
+var makeEarth = function()
+{
+	var position = vec();
+	var radius = 6.371e6;
+	return {
+		r: function() {return radius;},
+		tick: function(){},
+		pos: function() {return position;},
+		style: function(){return "blue";}
+	};
+};
 var makeView = function(start)
 {
 	var pos = start;
@@ -64,12 +67,7 @@ var makeView = function(start)
 		n= u.cross( v );
 	};
 
-	var moves = [
-		{v:vec({x:0,y:0,z:5e5}),a:87,s:83}, // w s
-		{v:vec({x:5e5,y:0,z:0}),a:68,s:65}, // d a
-		{v:vec({x:0,y:5e5,z:0}),a:81,s:69}  // q e
-	];
-
+	var movespeed = 1e6;
 	var rots = [
 		{t:"x",a:73,s:75}, // i k
 		{t:"y",a:74,s:76}, // j l
@@ -80,6 +78,12 @@ var count = 0;
 		rotate: rotate,
 		tick: function(keys)
 		{
+			var moves = [
+				{v:n.mul(movespeed),a:87,s:83}, // w s
+				{v:u.mul(movespeed),a:68,s:65}, // d a
+				{v:v.mul(movespeed),a:81,s:69}  // q e
+			];
+
 			var angle = 0.001*2*Math.PI;
 			for( var k = 0; k < keys.length; k++ )
 			{
@@ -124,8 +128,6 @@ var count = 0;
 			{
 				var x = p.dot( this.u());
 				var y = p.dot( this.v());
-		//		if( count % 100 === 0 )
-		//			console.log(ndist+"|"+x+"|"+y);
 				var cx = (x*fov) / (ndist+fov) + w/2;
 				var cy = (y*fov) / (ndist+fov) + h/2;
 
@@ -141,82 +143,111 @@ var count = 0;
 	};
 };
 
-var makeParticle = function( energy, position, velocity )
+// energy decides particle speed, velocity is only the unit vector for the velocity direction
+var makeParticle = function( energy, position, velocity, halt )
 {
-	var scaler = makeScaler();
-	var eV = 1.60218e-19; //          %En elektronvolt [J]
-	var MJ = 3844e5;     //          %Avstånd mellan månen och jorden [m]
-	var RE = 6.378e6;        //      %Jordradien [m]
-	var MR = 1.7371e6;        //     %Månradien [m]
-	var c = 3e008;               //    %Ljusets hastighet [m/s]
+	var t89c = makeT89C();
+	var eV = 1.60218e-19;
+	var RE = 6.378e6;
+	var c = 3e008;
 	var c2 = c*c;
-	var q = 1.602e-19;            //   %Elementarladdning för proton [C]
-	var m = 1.6726e-27;           //   %Protons massa [kg]
+	var q = 1.602e-19;
+	var m = 1.6726e-27;
+	var style = "#77F";
+	var radius = 7e5;
 
-//	dt = 1e-003;         //        %Tidssteg [s]
-	var E = energy || 3e5*1e6*eV;  //        %Energi [J]
 
-	var iopt = 3;       //             %Värde beroende av aktivitet där ca 3 är medel
-	var ps = 0;         //             %Ingen lutning av dipol
+	var E = energy || 3e5*1e6*eV;
+
+	var iopt = 3;
+	var ps = 0;
 	var parmod = []; // 10
-	var emc = (E/(m*c2)+1)
-	var absv = c*Math.sqrt( 1 - 1/(emc*emc)); //     %Hastighetens belopp [m/s]
-	var g = 1/Math.sqrt(1-(absv*absv)/c2); //               %Gamma
+	var emc = (E/(m*c2)+1);
+	var absv = c*Math.sqrt( 1 - 1/(emc*emc));
+	var g = 1/Math.sqrt(1-(absv*absv)/c2);
 
-	//M is end posit
-	var theta = 0; // radians
-	var r = position;//vec({x:-MJ+(MJ+10*RE)*Math.cos(theta),y:(MJ+10*RE)*Math.sin(theta),z: 0}); //  %Postition [m]
-	var v = vec({
-		x: absv*(Math.random()-0.5),
-		y:absv*(Math.random()-0.5),
-		z: absv*(Math.random()-0.5)
-	});//    %Hastighet [m/s]
-	var p = v.mul(m*g); //               %Rörelsemängd [kg*m/s]
+	var r = position;
+	var v = (velocity || vec({
+		x: (Math.random()-0.5),
+		y:(Math.random()-0.5),
+		z: (Math.random()-0.5)
+	}).unit()).mul(absv);
+	var p = v.mul(m*g);
 
 	var ef_pos = position;
+	var we_are_done = false;
 
 	return {
 		pos: function() { return ef_pos; },
 		tick: function(dt) {
+			if( we_are_done )
+			{
+				return
+			}
 
-			var B_field = vec({x:0.3,y:0.1,z:0.5});
-			/*			if ( r[0] >= 5*RE )
-			 {
-			 B_field = vec();
-			 }
-			 else
-			 {
-			 //[bx,by,bz] = T89C(iopt,parmod,ps,r(1)/RE,0,r(3)/RE);
-			 //B_field = [bx,by,bz]*1e-009;
-			 }*/
+			if( halt !== undefined )
+			{
+				we_are_done = halt( this.pos() );
+				if( we_are_done )
+				{
+					style = "red";
+					radius = 1e5;
+				}
+			}
+
+			var t_b = t89c( iopt,parmod,ps, r.x()/RE, r.y()/RE, r.z()/RE );
+			if( t_b.bx === NaN )
+				t_b.bx = 0;
+			if( t_b.by === NaN )
+				t_b.by = 0;
+			if( t_b.bz === NaN )
+				t_b.bz = 0;
+			var B_field = vec({x:t_b.bx,y:t_b.by,z:t_b.bz}).mul( 1e-9 );
+
 			var A = v.mul(q);
 			var F = A.cross(B_field);
 
-			p = p.add( F.mul(dt) );          //            %Rörelsemängd [kg*m/s]
-			var pabs = p.abs(); // %Absolutbeloppet av rörelsemängd [kg*m/s]
-			v = p.mul(1/Math.sqrt(m*m+(pabs*pabs)/c2));  //  %Hastigheten [m/s]
-			r = r.add(v.mul(dt) );               //       %Punkt: r=r+dr [m]
+			p = p.add( F.mul(dt) );
+			var pabs = p.abs();
+			v = p.mul(1/Math.sqrt(m*m+(pabs*pabs)/c2));
+			r = r.add(v.mul(dt) );
 
 
 			ef_pos = vec({x:r.x(),y:r.y(),z:r.z()});
 		},
-		r: function(){ return 7e5;},
-		style: function(){return "#77F"}
+		r: function(){ return radius;},
+		style: function(){return style;}
 	};
 };
 
 var makeMoonSim = function()
 {
-	var scaler = makeScaler();
-	var viewport = makeView(vec({x:50,y:-50,z:-80e6}));
 	var drawables = [];
+	var moon = makeMoon();
+	var earth = makeEarth();
+	var viewport = makeView(moon.pos().add(vec({x:0,y:0,z:-10e6})));
 
 	for( var i = -100e6; i< 100e6; i+=34e6 )
 		for( var j = -100e6; j< 101e6; j+=35e6 )
 			for( var k = -100e6; k< 101e6; k+=36e6 )
-				drawables.push(makeParticle(undefined,vec({x:i,y:j,z:k})));
-	var moon = makeMoon();
+			{
+				var part_pos = moon.pos().add(vec({x:i,y:j,z:k}));
+				drawables.push(makeParticle(
+					undefined,
+					part_pos,
+					moon.pos().sub(part_pos).unit(),
+					function(p){
+					if(p.sub(moon.pos()).abs() < moon.r() )
+					{
+						return true;
+					}
+					return p.sub(earth.pos()).abs() < earth.r();
+
+
+				}));
+			}
 	drawables.push(moon);
+	drawables.push(earth);
 	var lastmark = -1;
 	var sortFunction = function(a,b){
 		var v1 = b.pos().sub( viewport.pos() ).abs();
@@ -231,152 +262,14 @@ var makeMoonSim = function()
 		viewport.tick( keys );
 		for( var d in drawables )
 		{
-			drawables[d].tick( scaler.time(mark - lastmark) );
+			drawables[d].tick( (mark - lastmark)/10000 );
 		}
 		drawables.sort( sortFunction );
 
-		for( var d in drawables )
+		for( var d2 in drawables )
 		{
-			viewport.draw( context, width,height, drawables[d] );
+			viewport.draw( context, width,height, drawables[d2] );
 		}
-		//document.getElementById("fps").innerText =
-		//	"vp: "+viewport.pos().info() + " n:"+viewport.n().info();//+
-//			"\nm: "+moon.pos().info() +
-//			"\np: "+part.pos().info();
-
 		lastmark = mark;
 	};
 };
-
-var makeColorer = function( visible, gone )
-{
-	var k = -255/(gone-visible);
-	var m = 255 - k*visible;
-	return function( distance ) {
-		if( distance < visible )
-			return 255;
-		if( distance > gone )
-			return 0;
-		return Math.floor(k*distance+m);
-	};
-};
-
-var setifundefined = function( object, name, value )
-{
-	if( object[name] === undefined )
-		object[name] = value;
-};
-
-var combiner = function(c,r,g,b)
-{
-	return "rgb(" + Math.round(c*r) + "," + Math.round(c*g) + "," + Math.round(c*b) + ")";
-};
-
-var makeStar = function( stardata, settings )
-{
-	if( stardata === undefined )
-		stardata = {};
-	if( settings === undefined )
-		settings = {};
-
-	setifundefined( stardata, "starsize", 10 );
-	setifundefined( stardata, "colorredbias", 1- Math.random()*0.2  );
-	setifundefined( stardata, "colorbluebias", 1- Math.random()*0.2 );
-	setifundefined( stardata, "colorgreenbias", 1- Math.random()*0.2 );
-	setifundefined( stardata, "starheight", Math.random() + 0.3 );
-	setifundefined( stardata, "startposition", Math.random() );
-	setifundefined( stardata, "rotationspeed", (Math.random() - 0.5) * 0.0007 );
-	setifundefined( stardata, "starspeed", Math.random() / 1000  +.0001 );
-	setifundefined( stardata, "startangle",  Math.random() * 2 * Math.PI );
-
-	setifundefined( settings, "colorminposition", .2 );
-	setifundefined( settings, "colormaxposition", .9 );
-	setifundefined( settings, "viewportheight", .5 );
-	setifundefined( settings, "viewportposition", .15 );
-	setifundefined( settings, "maxstarheight", 0.9 );
-
-	var colorer = makeColorer( settings.colorminposition, settings.colormaxposition );
-
-	var polar = {
-		angle: 0,
-		depth: 0,
-		_length: null,
-		length: function(){
-			if(this._length === null )
-				this._length = stardata.starheight * settings.viewportposition / this.depth;
-			return this._length;
-		},
-		_hyp: null,
-		hyp: function(){
-			if(this._hyp===null)
-				this._hyp= Math.sqrt(this.depth*this.depth + stardata.starheight*stardata.starheight);
-			return this._hyp;
-		}
-	};
-	var conformer = function(min,max,diff){
-		return function( mark, start ){
-			var delta = max-min;
-			return (mark*diff + start) % delta;
-		};
-	};
-
-	var conformdepth = conformer( 0, 1, -1* stardata.starspeed );
-	var conformangle = conformer( 0, 2*Math.PI,stardata.rotationspeed);
-
-	var tick = function( mark ){
-
-
-		polar.angle = conformangle( mark,stardata.startangle );
-		//stardata.rotationspeed;
-		if( polar.angle < 0 )
-			polar.angle += 2*Math.PI;
-		else if( polar.angle > 2 * Math.PI )
-			polar.angle -= 2*Math.PI;
-
-		polar.depth = conformdepth(mark,stardata.startposition);//stardata.starspeed;
-		if( polar.depth < 0 )
-			polar.depth += 1;
-		else if( polar.depth > 1 )
-			polar.depth -= 1;
-		polar._hyp = null;
-		polar._length = null;
-	};
-	var color = function(){
-		var val = colorer(polar.hyp());
-		return combiner(val,stardata.colorredbias,stardata.colorgreenbias,stardata.colorbluebias);
-	};
-	var starsize = function(){
-		var val = (settings.viewportheight/settings.viewportposition)*polar.depth;
-		var ratio = (stardata.starsize/val)*settings.viewportheight;
-		return ratio;
-	};
-	var cartesian = {
-		x: function(){
-			return polar.length() * Math.cos( polar.angle )
-		},
-		y: function(){
-			return polar.length() * Math.sin( polar.angle )
-		}
-	};
-	return {
-		draw: function(context, canvaswidth, canvasheight, mark) {
-			tick(mark);
-			context.fillStyle = this.color();
-			if( polar.length() < Math.sqrt(2) )
-			{
-				context.beginPath();
-				context.arc(
-					Math.floor((cartesian.x()+0.5)*canvaswidth),
-					Math.floor((cartesian.y()+0.5)*canvasheight),
-					starsize(), 0, Math.PI*2, true);
-				context.fill();
-			}
-		},
-		cartesian: cartesian,
-		polar: polar,
-		color: color,
-		tick : tick,
-		starsize: starsize
-	};
-};
-
